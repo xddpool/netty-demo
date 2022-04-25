2351be48e83ecafc1f8d6f40509751cee5c40292
xddpool <781488631@qq.com>
2022-04-25 16:36:07 +0800

    RpcServerClient

--- src/main/java/cn/itcast/server/RpcServer.java
+++ src/main/java/cn/itcast/server/RpcServer.java
@@ -2,33 +2,39 @@ package cn.itcast.server;
 
 import cn.itcast.protocol.MessageCodecSharable;
 import cn.itcast.protocol.ProtocolFrameDecoder;
+import cn.itcast.server.handler.RpcRequestMessageHandler;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.ServerSocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class ChatServer {
+public class RpcServer {
     public static void main(String[] args) {
+
         NioEventLoopGroup boss = new NioEventLoopGroup();
         NioEventLoopGroup worker = new NioEventLoopGroup();
         LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
         MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        RpcRequestMessageHandler RPC_HANDLER = new RpcRequestMessageHandler();
+
+
         try {
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.channel(NioServerSocketChannel.class);
             serverBootstrap.group(boss, worker);
-            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            serverBootstrap.handler(new ChannelInitializer<ServerSocketChannel>() {
                 @Override
-                protected void initChannel(SocketChannel ch) throws Exception {
+                protected void initChannel(ServerSocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new ProtocolFrameDecoder());
                     ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    ch.pipeline().addLast(RPC_HANDLER);
                 }
             });
             Channel channel = serverBootstrap.bind(8080).sync().channel();
